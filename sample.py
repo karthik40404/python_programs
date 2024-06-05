5ed329a0145b4949cb6d211b28c250070c59151b
karthik40404 <karpython123@gmail.com>
2024-06-05 11:20:56 +0530

    message

--- sample.py
+++ sample.py
@@ -25,7 +25,50 @@ h={10,20}
 print(type(h))
 i={'key':'value'}
 print(type(i))
-age=int(input('enter age'))
-name=str(input('enter name'))
-weight=float(input('enter weight'))
-print(age,name,weight)
+# age=int(input('enter age'))
+# name=str(input('enter name'))
+# weight=float(input('enter weight'))
+# print(age,name,weight)
+print (10+2)
+print("welcome "+"home")
+print(10-3)
+print(5*3)
+print(10/4)
+print(10%2)
+print(10**3+2**5)
+print(10//3)
+print(10==10)
+print(10==11)
+print(10!=10)
+print(10!=11)
+print(10>5)
+print(10<5)
+print(10<=6)
+print(10>=7)
+a=10
+print(a)
+b=20
+b+=10
+print(b)
+
+print(10==10 and 10>=8 and 23<=3)
+print(10==10 or 10>=7)
+print(not(True))
+
+q="wlecome"
+print('e'in q)
+print('e'not in q)
+print('z'in q)
+print('z'not in q)
+print('identity operator')
+a=10
+b=10
+print(a is b)
+print(id(a))
+print(id(b))
+
+a=[10,20]
+b=[10,20]
+print(id(a))
+print(id(b))
+print(a is b)
