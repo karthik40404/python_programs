d082090277d4ec24b5a41e7bbef0b13b20dd1e39
karthik40404 <karpython123@gmail.com>
2024-06-05 09:48:49 +0530

    message

---
+++ sample.py
@@ -0,0 +1,31 @@
+print('welcome')
+age=21
+name='jones'
+weight=50
+print('age :',age)
+print('name:',name)
+print('weight:',weight)
+a=10 
+print(type(a))
+b=20.5
+print(type(b))
+c=2+4j
+print(type(c))
+name="bill"
+print(type(name))
+d=True
+print(type(d))
+e=False
+print(type(e))
+f=[10,20]
+print(type(f))
+g=(10,20)
+print(type(g))
+h={10,20}
+print(type(h))
+i={'key':'value'}
+print(type(i))
+age=int(input('enter age'))
+name=str(input('enter name'))
+weight=float(input('enter weight'))
+print(age,name,weight)
