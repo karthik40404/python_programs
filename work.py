2d8d6240db14bf77a1cee752332ed6bd43e27d8e
karthik40404 <karpython123@gmail.com>
2024-06-06 11:27:58 +0530

    message

---
+++ work.py
@@ -0,0 +1,59 @@
+a=int(input('enter a number'))
+b=int(input('enter another number'))
+print('sum:',a+b)
+
+a=int(input('enter a number'))
+b=int(input('enter another number'))
+print('sub:',a-b)
+
+a=int(input('enter a number'))
+b=int(input('enter another number'))
+print(a*b)
+
+a=int(input('enter a number'))
+b=int(input('enter another number'))
+print(a/b)
+
+a=int(input('enter a number'))
+b=int(input('enter another number'))
+print(a%b)
+
+a=int(input('enter a number'))
+b=int(input('enter another number'))
+print(a**b)
+
+a=int(input('enter a number'))
+b=int(input('enter another number'))
+print(a//b)
+
+a=int(input('enter a number'))
+b=int(input('enter another number'))
+print(a==b)
+
+a=int(input('enter a number'))
+b=int(input('enter another number'))
+print(a!=b)
+
+a=int(input('enter a number'))
+b=int(input('enter another number'))
+print(a>b)
+
+a=int(input('enter a number'))
+b=int(input('enter another number'))
+print(a<b)
+
+a=int(input('enter a number'))
+b=int(input('enter another number'))
+print(a>=b)
+
+a=int(input('enter a number'))
+b=int(input('enter another number'))
+print(a<=b)
+
+a=str(input('enter an item',))
+b=float(input('enter a price'))
+print("item is ",a,"and its price is",b)
+print("item is ",a," \n and \n its price is",b)
+print(f"""item is {a} 
+       and 
+      its price is {b}""")
