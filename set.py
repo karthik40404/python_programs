221b6098238ebb5c3672d3cb1e903b016c69ebd0
karthik40404 <karpython123@gmail.com>
2024-06-28 11:36:27 +0530

    mmes

---
+++ set.py
@@ -0,0 +1,74 @@
+# s={1,2,3,4}
+# print(type(s))
+
+# s=set() #emptyset
+# print(type(s))
+
+# s={1,2,3,4}
+# for i in s:
+#     print(i)
+
+# s={1,2,3,4,5}
+# num=int(input("enter a number :"))
+# if num in s:
+#     print("here")
+# else:
+#     print("not here")
+
+# s1={1,2,3,4,5,6,7,8}
+# s2={9,10,11,12,13,14,15,16}
+# s1.add(20)
+# print(s1)
+
+# s2={9,10,11,12,13,14,15,16}
+# s3=s2.copy()
+# print(s3)
+
+# s1={1,2,3,4,5,6,7,8,9,10,11}
+# s2={9,10,11,12,13,14,15,16}
+# s3=s2.difference(s1)
+# print(s3)
+
+# s1={1,2,3,4,5,6,7,8,9,10,11}
+# s2={9,10,11,12,13,14,15,16}
+# s1.discard(s2)
+# s2.remove(9)
+# print(s2)
+
+# s1={1,2,3,4,5,6,7,8,9,10,11}
+# s2={9,10,11,12,13,14,15,16}
+# s1.isdisjoint(s2)
+# print(s1)
+# s2.isdisjoint(s1)
+# print(s2)
+
+# s1={1,2,3,4,5,6,7,8,9,10,11}
+# s2={9,10,11,12,13,14,15,16}
+# print(s1.issubset(s2))
+# print(s2.issubset(s1))
+
+# s1={0,1,2,3,4,5,6,7,8,9,10,11}
+# s2={9,10,11,12,13,14,15,16}
+# s1.pop()
+# print(s1)
+
+# s1={0,1,2,3,4,5,6,7,8,9,10,11}
+# s2={9,10,11,12,13,14,15,16}
+# s3=s1.symmetric_difference({1,2,3})
+# print(s3)
+
+# l=[1,2,3,3,4,3,5,6,7,8]
+# s=set(l)
+# #print(s)
+# l=list(s)
+# print(l)
+
+python=set()
+mern=set()
+while True:
+    limit=int(input("enter the limit for python :",))
+    len(python)
+    limit=int(input("enter the limit for mern 5:",))
+    len(mern)
+    
+
