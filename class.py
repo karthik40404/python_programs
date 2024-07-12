f811d425208211171ee89deb54cd8f2106f888e6
karthik40404 <karpython123@gmail.com>
2024-07-12 11:25:46 +0530

    m

---
+++ class.py
@@ -0,0 +1,27 @@
+# class ss:
+#     def cs():
+#         print('cs')
+#     def ca():
+#         print('ca')
+
+# ani=ss
+# ani.cs()
+# ani.ca()
+
+# class ss:
+#     def cs(s):
+#         s.database='py'
+#         print('cs')
+#     def ca(self):
+#         print('ca',self.database)
+
+# ani=ss()
+# ani.cs()
+# ani.ca()
+# print(ani.database)
+
+class ss:
+    def __init__(self) :
+        print('register')
+ani=ss()
+
--- except_handle.py
+++ except_handle.py
@@ -14,11 +14,22 @@
     # except:
     #     print('input a int')
 
-l=[1,2,3,4,'haha',9]
-sum=0
-for i in l:
-    try:
-       sum+=i
-    except:
-        pass
-print(sum)
+# l=[1,2,3,4,'haha',9]
+# sum=0
+# for i in l:
+#     try:
+#        sum+=i
+#     except:
+#         pass
+# print(sum)
+
+try:
+    print('1'+'abc')
+except NameError:
+    print('namerror')
+except TypeError:
+    print('typerror')
+else:
+    print('else')
+finally:
+    print('final')
