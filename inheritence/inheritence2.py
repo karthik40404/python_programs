12e014218905f3e290bf1ccb1fccf34052b2dfed
karthik40404 <karpython123@gmail.com>
2024-07-15 23:25:33 +0530

    project

--- inheritence/inheritence2.py
+++ inheritence/inheritence2.py
@@ -1,9 +1,3 @@
-# Person (Base Class)
-# Employee (Derived from Person) - Single Inheritance
-# Manager (Derived from Employee) - Multilevel Inheritance
-# DepartmentHead (Derived from Manager and Employee) - Multiple Inheritance
-# HRManager (Derived from DepartmentHead) - Hierarchical Inheritance
-
 class person():
     def __init__(self) -> None:
         print('register')
