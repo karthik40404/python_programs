5b66c97fb5da6ba4630f03b1d711ea3445292c2f
karthik40404 <karpython123@gmail.com>
2024-08-07 01:40:21 +0530

    task

--- newtask.py
+++ newtask.py
@@ -10,18 +10,23 @@
 
 # main()
 
-def swap_numbers_list(a, b):
-    numbers = [a, b]
-    numbers[0], numbers[1] = numbers[1], numbers[0]
-    return numbers[0], numbers[1]
+# def swap_numbers_list(a, b):
+#     numbers = [a, b]
+#     numbers[0], numbers[1] = numbers[1], numbers[0]
+#     return numbers[0], numbers[1]
 
-def main():
-    a = int(input("Enter the first number (a): "))
-    b = int(input("Enter the second number (b): "))
-    print(f"Before swapping: {a}{b}")
-    a, b = swap_numbers_list(a, b)
-    print(f"After swapping:{a}{b}")
+# def main():
+#     a = int(input("Enter the first number (a): "))
+#     b = int(input("Enter the second number (b): "))
+#     print(f"Before swapping: {a}{b}")
+#     a, b = swap_numbers_list(a, b)
+#     print(f"After swapping:{a}{b}")
 
 
-main()
+# main()
 
+a = int(input("Enter the first number (a): "))
+b = int(input("Enter the second number (b): "))
+a, b = b, a
+print('a =', a)
+print('b =', b)
